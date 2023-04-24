bf3cd803818ccf72a97d4bfe20d054d0a0542bdf
Luis Hernandez <lic.luish05@gmail.com>
2023-04-24 17:04:06 -0500

    corrigiendo

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import logo from "./assets/image/logo minga.png"
-import menu from "./assets/image/menu hamburguesa.png"
+import logo from "./assets/image/logo-minga.png"
+import menu from "./assets/image/menu-hamburguesa.png"
 import naruto from "./assets/image/imagen1.png"
 import portada from "./assets/image/image 2.png"
 import flecha1 from "./assets/image/flecha1.png"
@@ -7,9 +7,11 @@ import flecha2 from "./assets/image/flecha2.png"
 import footer from "./assets/image/bf footer.png"
 import redes from "./assets/image/redes.png"
 import './App.css'
+import apiUrl from "../api"
 
 function App() {
-
+console.log(apiUrl);
+console.log(process.env.NODE_ENV);
   return (
     <>
     <header>
@@ -100,3 +102,24 @@ function App() {
 }
 
 export default App
+
+
+{/* <div>
+        <button className="text-white not-italic font-medium text-2xl leading-[95.19%] bg-gradient-to-r from-btn1 from-(-13.9%) to-btn2 to-58.69% rounded-md flex flex-row justify-center items-center gap-2.5 w-60 h-[55px] p-4">
+                    Donate
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                      />
+                    </svg>
+        </button>
+      </div> */}
