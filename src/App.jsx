640367b95121ec1bac5e1bde12e9dbcda5bfad32
Luis Hernandez <125385157+LuisH05@users.noreply.github.com>
2023-04-24 21:21:07 -0500

    M 022 (#4)
    * minga v1
    
    * commit 3
    
    * modificando
    
    * f-01
    
    * corrigiendo
    
    * m-02
    
    * modificando

--- src/App.jsx
+++ src/App.jsx
@@ -15,25 +15,31 @@ console.log(apiUrl);
 console.log(process.env.NODE_ENV);
   return (
     <>
-    <header>
-    <nav className="w-full flex justify-between items-center mx-auto px-8 m-8 xsm:hidden">
+
+    <header className="mx-10">
+    <nav className="w-full flex justify-between items-center xsm:hidden mt-10">
+
       <img src={menu} className="w-[3rem]"/>
-      <img src={logo} className="w-[5rem]"/>
+      <img src={logo} className="w-[4rem]"/>
     </nav>
 
-    <div className="flex justify-between items-end  w-full h-[18rem] bg-gradient-to-l from-pink-400 via-pink-400 to-pink-300 my-20 mx-[rem] xsm:hidden ">
 
-    <div className=" absolute w-[100%] flex content-center my-28 justify-between">
-      <img src={flecha1} className="w-[3rem]"/>
-      <img src={flecha2} className="w-[3rem]"/>
-    </div>
-    <div className="w-[100%] h-[150%] flex">
+    <div className="flex justify-between items-center w-full h-[16rem] bg-gradient-to-l from-pink-400 via-pink-400 to-pink-300 my-8 rounded-md xsm:hidden ">
 
-    <div className="w-[50%]">
-    <img src={naruto} className="w-auto h-[80%] my-24 "/>
-    </div>
-    <div className="w-[50%]">
-    <img src={portada} className="w-auto h-[75%] my-20 "/>
+      <div className='absolute w-[90%] flex content-center my-28 justify-between ' >
+        <img src={flecha1} className="w-[3rem]" />
+        <img src={flecha2} className="w-[3rem]"/>
+      </div>
+
+
+    <div className="w-[100%] h-[22rem] flex">
+      <div className="w-[50%]">
+        <img src={naruto} className="w-auto h-[80%] my-4 "/>
+      </div>
+      <div className="w-[50%]">
+        <img src={portada} className="w-auto h-[75%] my-3 "/>
+      </div>
+
     </div>
     </div>
 
@@ -41,18 +47,23 @@ console.log(process.env.NODE_ENV);
     <h4 className="text-white text-2xl text-start w-[50%] pl-4">Shonen</h4>
     <p className="text-white text-sm text-justify w-[70%] pl-4">Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
 
+      <div className="h-auto w-[90%] ml-10">
+        <h4 className="text-white text-2xl text-start w-[50%] pl-2">Shonen</h4>
+        <p className="text-white text-sm text-justify w-[60%] pl-2 ">Is the manga that is aimed at adolescent boys. They are series with large amounts of action, in which humorous situations often occur. The camaraderie between members of a collective or a combat team stands out.</p>
+      </div>
     </div>
     </div>
     </header>
-
-    <div className="bg-[url(/src/assets/image/bg-main.png)] bg-no-repeat bg-cover bg-center w-fill h-[551px] rounded-md mt-[3%] flex flex-col justify-center items-start pl-[10%] gap-[15px] p-0 xsm:hidden">
+    
+    <div className="bg-[url(/src/assets/image/bg-main.png)] bg-no-repeat bg-cover bg-center w-fill h-[500px] rounded-md mt-[1] flex flex-col justify-center items-start pl-[10%] gap-[10px] p-0 xsm:hidden mx-10">
             <h2 className="not-italic font-bold text-[64px] leading-[95.19%] text-white text-shadow: 1px 8px 50px rgba(255, 255, 255, 0.25)">Live the emotion of the manga</h2>
             <p className="font-normal text-2xl leading-[95.19%] text-white">Find the perfect manga for you</p>
             <span className="not-italic font-semibold text-base leading-[95.18%] text-white">#MingaForever 🔥</span>
             <button className="text-white not-italic font-medium text-2xl leading-[95.19%] bg-gradient-to-r from-btn1 from-(-13.10%) to-btn2 to-58.69% rounded-md flex flex-row justify-center items-center gap-2.5 w-60 h-[55px] p-4">Sign In!</button>
     </div>
 
-    <footer className="w-auto my-8 xsm:hidden">
+    <footer className="w-auto my-8 xsm:hidden mx-10">
+
     <div className="">
       <img src={footer} className="w-[100%]"/>
     </div>
@@ -77,8 +88,10 @@ console.log(process.env.NODE_ENV);
 
 
     {/* //mobile */}
-    <div className="flex justify-center xsm:h-screen sm:hidden">
-      <div className="xsm:bg-[url(/src/assets/image/bg-mobile.png)] xsm:w-full xsm:h-full xsm:bg-cover">
+
+    <div className="flex justify-center xsm:h-full">
+      <div className="xsm:bg-[url(/src/assets/image/bg-mobile.png)] xsm:w-full xsm:h-screen xsm:bg-cover md:hidden">
+
         <div className="flex justify-center items-center h-20">
         <div className="flex w-full mx-4 justify-between">
         <div className="flex items-center">
@@ -87,7 +100,9 @@ console.log(process.env.NODE_ENV);
               </svg>
         </div>
         <div>
-          <img src={logo} className="xsm:w-12"/>
+
+          <img src={logo} className="xsm:w-12 "/>
+
         </div>
         </div>
         </div>
@@ -96,7 +111,9 @@ console.log(process.env.NODE_ENV);
         <h1 className="text-white xsm: text-4xl xsm:text-center xsm:font-bold">Live the emotion of the manga</h1>
         <h3 className="text-white xsm: self-center xsm:text-base xsm:mb-2 p-3">Find the perfect manga for you</h3>
         <div className="xsm: self-center">
-          <button className="rounded-3xl text-white bg-gradient-to-b from-[#F9A8D4] to-[#F472B6] h-10 w-64">Explore</button>
+
+          <button className="rounded-3xl text-white bg-gradient-to-b from-[#F9A8D4] to-[#F472B6] h-10 w-64 " >Explore</button>
+
         </div>
         </div>
       </div>
@@ -127,5 +144,6 @@ export default App
                       />
                     </svg>
         </button>
+
       </div> */}
 
