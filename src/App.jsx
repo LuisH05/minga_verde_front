76a01f46aebfd7b4b4e13b93e1033b6afa4d2941
Luis Hernandez <lic.luish05@gmail.com>
2023-04-24 21:34:26 -0500

    modifi

--- src/App.jsx
+++ src/App.jsx
@@ -106,7 +106,6 @@ console.log(process.env.NODE_ENV);
 
 export default App
 
-
 {/* <div>
         <button className="text-white not-italic font-medium text-2xl leading-[95.19%] bg-gradient-to-r from-btn1 from-(-13.9%) to-btn2 to-58.69% rounded-md flex flex-row justify-center items-center gap-2.5 w-60 h-[55px] p-4">
                     Donate
